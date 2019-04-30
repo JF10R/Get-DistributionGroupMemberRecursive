31af0dbd805dee4814344880f84cc1138fd6bc59
Jeff Noel <jfnoel@jfnoel.com>
2019-04-30 10:38:39 -0400

    Ease of use change to required parameter
    Instead of providing the group identity, the user only needs to provide the group name.

--- script.ps1
+++ script.ps1
@@ -1,6 +1,7 @@
 
-function Get-DistributionGroupMemberRecursive ($GroupIdentity) {
-  $member_list = Get-DistributionGroupMember -Identity $GroupIdentity
+function Get-DistributionGroupMemberRecursive ($GroupName) {
+  $group = Get-DistributionGroup $GroupName
+  $member_list = Get-DistributionGroupMember -Identity $group.Identity
   foreach ($member in $member_list) {
     if ($member.RecipientType -like '*Group*') {
        Get-DistributionGroupMemberRecursive -GroupIdentity $member.Identity
@@ -10,4 +11,4 @@ function Get-DistributionGroupMemberRecursive ($GroupIdentity) {
   }
 }
 #$group = Get-DistributionGroup "GroupName"
-#Get-DistributionGroupMemberRecursive -GroupIdentity $group.Identity | Export-csv -Path C:\test.csv -Delimiter ';' -Encoding 'UTF8'
+#Get-DistributionGroupMemberRecursive -Group $group.Identity | Export-csv -Path C:\test.csv -Delimiter ';' -Encoding 'UTF8'
